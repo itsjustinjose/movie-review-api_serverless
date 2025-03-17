d05f818cb859aef8aad14f0692fa21a8020a5c94
itsjustinjose <itsjustinjose@gmail.com>
2025-03-17 10:45:02 +0000

    Setup for Auth APII.

--- bin/movie_review.ts
+++ bin/movie_review.ts
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { LambdaCDKStack } from '../lib/cdk-stack';
-// import { ApiStack } from '../lib/api-stack';
-// import { AuthCDKStack } from '../lib/authcdkStack';
+import { AuthAppStack } from '../lib/auth-app-stack';
 
 
 const app = new cdk.App();
 
 
 const lstack = new LambdaCDKStack(app, "lstack")
+new AuthAppStack(app, 'AuthAPIStack');
